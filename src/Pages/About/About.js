b6f59150c2e9733e0b527147e0b879cf5930cd84
rahul palamarthi <rahulpalamarthi@gmail.com>
2023-11-06 16:29:09 +0530

    feat: change title and update about page links

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,39 +1,32 @@
+import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import './About.css';
 
 export default function About() {
 	return (
-		<>
+		<Fragment>
 			<section className='about'>
 				<div className='about-wrapper'>
 					<p className='about-intro'>
-						Hi, my name is Rahul Palamarthi. I'm a Enthusiastic, Hungry Developer and Designer from India.Since 2021 I have been working on Open
+						Hi, my name is Rahul Palamarthi. I'm a Enthusiastic, Hungry Developer and Designer from India. Since 2021 I have been working on Open
 						Source Projects.
 					</p>
 					<p className='about-outro'>This is my Playgrounds where you find all my Projects.Hope you will enjoy it.</p>
 					<strong>{'>_'} Rahul Palamrthi</strong>
 					<ul className='about-links-wrapper'>
 						<li>
-							<a href='mailto:rahulpalamarthi@gmail.com'>gmail</a>
-						</li>
-						<li>
-							<Link to={'https://github.com/Rahul-Palamarthi'} target='_blank'>
+							<Link to={'https://github.com/rahu1gg'} target='_blank' rel='noreferrer'>
 								github
 							</Link>
 						</li>
 						<li>
-							<Link to={'https://www.linkedin.com/in/rahul-palamarthi/'} target='_blank'>
+							<Link to={'https://www.linkedin.com/in/rahul-palamarthi/'} target='_blank' rel='noreferrer'>
 								linkedin
-							</Link>
-						</li>
-						<li>
-							<Link to={'https://rahul-palamarthi.github.io/Web-Portfolio/'} target='_blank'>
-								portfolo
 							</Link>
 						</li>
 					</ul>
 				</div>
 			</section>
-		</>
+		</Fragment>
 	);
 }
